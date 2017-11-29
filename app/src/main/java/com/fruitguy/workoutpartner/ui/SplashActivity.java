bc1ffa1ebb591b009f9b23f9c2658eec90ac8c1b
kevin liao <he.liao@sfara.com>
2017-11-28 21:19:30 -0500

    Splash Screen

---
+++ app/src/main/java/com/fruitguy/workoutpartner/ui/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.fruitguy.workoutpartner.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by heliao on 11/27/17.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new CountDownTimer(3000, 3000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }.start();
+    }
+}
